529bc0acb6f8c42c4748e418a6de0ddaec5c2cae
Santiago Alvarez <alvarezsanjavier@gmail.com>
2026-03-17 14:45:03 -0300

    update: Logo component to use new branding image and alt text

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -16,14 +16,12 @@ export const Logo = (props: Props) => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
-      alt="Payload Logo"
-      width={193}
-      height={34}
+      alt="Javier González Grupo Logo"
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      className={clsx('max-w-[9.375rem] w-full h-auto', className)}
+      src="/logo-b&g.png"
     />
   )
 }
