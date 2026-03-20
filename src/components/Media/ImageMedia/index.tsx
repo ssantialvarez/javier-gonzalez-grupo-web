40dc82fe47312216f649b8bddc776096bb71210a
Santiago Alvarez <alvarezsanjavier@gmail.com>
2026-03-20 01:11:44 -0300

    feat: translate posts and pagination labels to Spanish

--- src/components/Media/ImageMedia/index.tsx
+++ src/components/Media/ImageMedia/index.tsx
@@ -93,7 +93,7 @@ export const ImageMedia: React.FC<MediaProps> = (props) => {
         height={!fill ? height : undefined}
         placeholder="blur"
         blurDataURL={placeholderBlur}
-        priority={priority}
+        priority={priority ?? true}
         quality={100}
         loading={loading}
         sizes={sizes}
